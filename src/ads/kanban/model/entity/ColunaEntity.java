872e8b434ed22b95a7df3ef07c8d801af8bec49c
rguirra <rodrigoguirratrindade@gmail.com>
2020-04-18 02:24:09 -0300

    Classe ColunaEntity

---
+++ src/ads/kanban/model/entity/ColunaEntity.java
@@ -0,0 +1,24 @@
+package ads.kanban.model.entity;
+
+public class ColunaEntity {
+	private int id_coluna;
+	private String titulo_coluna;
+	
+	public int getId() {
+		return id_coluna;
+	}
+	public void setId(int id) {
+		this.id_coluna = id;
+	}
+	public String getTitulo() {
+		return titulo_coluna;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo_coluna = titulo;
+	}
+	
+	public String toString() {
+		return "ColunaEntity [id=" + id_coluna + "titulo=" + titulo_coluna +"]";
+		
+	}
+}
