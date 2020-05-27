583a6579d531abbc28b4c301a977556b461279af
Lucas Souza <lucasouliveira@gmail.com>
2020-05-27 18:19:17 -0300

    🛠 Adicionado arraylist de tickets dentro de coluna entity

--- src/ads/kanban/model/entity/ColunaEntity.java
+++ src/ads/kanban/model/entity/ColunaEntity.java
@@ -1,13 +1,19 @@
 package ads.kanban.model.entity;
 
+import sun.security.krb5.internal.Ticket;
+
+import java.util.ArrayList;
+
 public class ColunaEntity {
 	private int id;
 	private String titulo;
 	private QuadroEntity quadro;
+	private ArrayList<TicketEntity> tickets;
 	
 	public QuadroEntity getQuadro() {
 		return quadro;
 	}
+
 	public void setQuadro(QuadroEntity quadro) {
 		this.quadro = quadro;
 	}
@@ -23,6 +29,15 @@ public class ColunaEntity {
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
+	public ArrayList<TicketEntity> getTickets() {
+		return tickets;
+	}
+
+	public void setTickets(ArrayList<TicketEntity> tickets) {
+		this.tickets = tickets;
+	}
+
 	@Override
 	public String toString() {
 		return "ColunaEntity [id_coluna=" + id + ", titulo_coluna=" + titulo + ", quadro=" + quadro + "]";
